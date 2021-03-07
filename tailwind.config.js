1c062a7f6325012b5fe1ed91f75184fc26602e4e
Oliver Otchere <oliverotchere4@gmail.com>
2021-03-07 22:20:24 +0000

    purged tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  purge: ['./public/index.html'],
+  purge: {
+    enabled : true,
+    content:['./public/index.html'],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
