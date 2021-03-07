9e4fa5b241b64fe9806ca76f95244d9411816970
Oliver Otchere <oliverotchere4@gmail.com>
2021-03-07 22:09:47 +0000

    added custom colors and text

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  purge: ['./public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors:{
+        intro : 'hsl(217, 28%, 15%)',
+        main: 'hsl(218, 28%, 13%)',
+        footer: 'hsl(216, 53%, 9%)',
+        testimonials : 'hsl(219, 30%, 18%)',
+        ccyan : 'hsl(176, 68%, 64%)',
+        cblue : 'hsl(198, 60%, 50%)'
+      },
+      fontFamily:{
+        heading:['Raleway'],
+        body:['Open Sans']
+      },
+      zIndex: {
+        neg:'-1'
+      },
+      backgroundImage: theme => ({
+        'hero-desktop': "url('images/bg-curvy-desktop.svg')",
+        'hero-mobile': "url('images/bg-curvy-mobile.svg')",
+        'comma':"url('images/bg-quotes.png')"
+       }),
+       backgroundSize: {
+        'comp':'100%',
+        'desktop-comp':'100% 30%'
+      },
+      width: {     
+        '30/30': '30%',
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
